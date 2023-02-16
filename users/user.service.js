606e50c2f174705b4f59bd43e86f11c51f7de0ef
Sadullah_David_Moses_TANRIKULU <96377202+Sadullah-TANRIKULU@users.noreply.github.com>
2023-02-16 14:27:22 +0300

    Update user.service.js

--- users/user.service.js
+++ users/user.service.js
@@ -2,7 +2,29 @@
 const jwt = require('jsonwebtoken');
 
 // users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const users = [
+    { 
+        id: 1, 
+        username: 'test', 
+        password: 'test', 
+        firstName: 'Test', 
+        lastName: 'User' 
+    }, 
+    { 
+        id: 2, 
+        username: 'test2', 
+        password: 'test2', 
+        firstName: 'Test2', 
+        lastName: 'User2' 
+    }, 
+    { 
+        id: 3, 
+        username: 'test3', 
+        password: 'test3', 
+        firstName: 'Test3', 
+        lastName: 'User3' 
+    }, 
+];
 
 module.exports = {
     authenticate,
